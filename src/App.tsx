dd76038ec85b6638542c17fe9e7fde5599c802f0
Birthright00 <117997217+Birthright00@users.noreply.github.com>
2024-07-08 09:32:57 +0800

    Added Login Page (Frontend)

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 import "./index.css";
 import { Tldraw } from "tldraw";
 import MyChatBot from "./components/chatbot/llm";
-
-import ChatBot from "react-chatbotify";
-
+import Login from './LoginPage';
 
 function App() {
+  // State to manage whether the user is logged in
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // Function to handle successful login
+  const handleLoginSuccess = () => {
+    setIsLoggedIn(true);
+  };
+
   return (
     <div>
-      <div className="tldraw">
-        <Tldraw />
-      </div>
-      <div className="chatbot">
-        <MyChatBot />
-
-        {/* <ChatBot /> */}
-
-      </div>
+      {!isLoggedIn ? (
+        <Login onLogin={handleLoginSuccess} />
+      ) : (
+        <>
+          <div className="tldraw">
+            <Tldraw />
+          </div>
+          <div className="chatbot">
+            <MyChatBot />
+          </div>
+        </>
+      )}
     </div>
   );
 }
