26c133dbf8e6e7ba15fe9627221ba3a9e914f6ab
yanzhenteo <115708948+yanzhenteo@users.noreply.github.com>
2024-07-12 04:19:31 +0800

    added llm file

---
+++ src/components/chatbot/llm.jsx
@@ -0,0 +1,91 @@
+import ChatBot from "react-chatbotify";
+import OpenAI from "openai";
+
+import React, { useState } from 'react';
+
+const mainColor = "#EFBE7B"
+
+
+const MyChatBot = () => {
+  let apiKey = null;
+  let modelType = "gpt-3.5-turbo";
+  let hasError = false;
+
+  // example openai conversation
+  // you can replace with other LLMs such as Google Gemini
+  const call_openai = async (params) => {
+    try {
+      const openai = new OpenAI({
+        apiKey: apiKey,
+        dangerouslyAllowBrowser: true // required for testing on browser side, not recommended
+      });
+
+      // for streaming responses in parts (real-time), refer to real-time stream example
+      const chatCompletion = await openai.chat.completions.create({
+        // conversation history is not shown in this example as message length is kept to 1
+        messages: [{ role: 'user', content: params.userInput }],
+        model: modelType,
+      });
+
+      await params.injectMessage(chatCompletion.choices[0].message.content);
+    } catch (error) {
+      await params.injectMessage("Unable to load model, is your API Key valid?");
+      hasError = true;
+    }
+  }
+  const flow={
+
+        options: {
+            openChat: {isOpen:false}
+        },
+        
+
+
+    start: {
+      message: "Ask me anything",
+      path: "loop",
+      isSensitive: false
+    },
+    // api_key: {
+    //   message: (params) => {
+    //     apiKey = params.userInput.trim();
+    //     return "Ask me anything!";
+    //   },
+    //   path: "loop",
+    // },
+    loop: {
+      message: async (params) => {
+        await call_openai(params);
+      },
+      path: () => {
+        if (hasError) {
+          return "start"
+        }
+        return "loop"
+      }
+    }
+  }
+
+  const buttonConfig ={
+    headerStyle:{background: mainColor},
+    tooltipStyle:{background: mainColor},
+    botBubbleStyle:{background: mainColor},
+    chatHistory: {storageKey: "example_llm_conversation"},
+    chatButton: {icon: "/capybara.png"},
+    tooltip: {text: "Ask Me Anything!😊 "},
+    botBubble:{showAvatar: true ,avatar: "/capybara.png"},
+    header: {
+      avatar: "/capybara.png",
+       title: "Capybara" ,
+    }
+       
+    }
+
+  return (
+    // options={{openChat: {isOpen:false}, theme: {embedded: true}, chatHistory: {storageKey: "example_llm_conversation"}}}
+    <ChatBot  options={buttonConfig} flow={flow}/>
+
+  );
+};
+
+export default MyChatBot;
