4281583132d7f87be15ba918a8c0e34a6e8906da
Darrel <Darrelliew6@gmail.com>
2024-07-28 22:22:52 +0800

    Login Page Finished

--- src/components/userDatabase/get_user.jsx
+++ src/components/userDatabase/get_user.jsx
@@ -1,21 +1,44 @@
-import React,{useEffect} from 'react'
+import React,{useEffect,useCallback} from 'react'
 import axios from 'axios'
 
 
-function GetUser() {    
-    useEffect(() => {
-        const fetchUser = async () => {
-            const response = await axios.get('http://localhost:3000/user/66a3bb91b1e3a7dcc42876d0');
-            console.log(response)
+export const validate = async(username,password) => {
+  await axios.get(`http:localhost:3000/${username}`)
+} 
 
-        };
+
+// const onLoginButtonClick = useCallback(async () => {
+//   try {
+//     const response = await axios.get(
+//       "http://localhost:3000/user/66a3bb91b1e3a7dcc42876d0"
+//     );
+//     const user = response.data;
+//     if (user.username === username && user.password === password) {
+//       navigate("/home");
+//     } else {
+//       setError("Invalid username or password");
+//     }
+//   } catch (err) {
+//     console.error("Login failed:", err);
+//     setError("Failed to log in");
+//   }
+// }, [username, password, navigate]);
+// function GetUser() {    
+//     useEffect(() => {
+//         const fetchUser = async () => {
+//           const requestOptions = {
+//             method: 'GET',
+//             headers: { 'Content-Type': 'application/json' },
+//             body: JSON.stringify(userData)
+//           }
+//             const response = await axios.get('http://localhost:3000/user',requestOptions);
+//             console.log(response)
+        
+
+//         };
     
-            fetchUser();
-        }, []);
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  )
-}
-export default GetUser
+//             fetchUser();
+//         }, []);
+//   return ()
+// }
+
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,21 @@
 import { FunctionComponent } from "react";
 import Cards from "../components/Cards";
 import NavBar from "../components/NavBar";
+import { useLocation } from "react-router-dom";
+import { UserWhiteboards } from "../components/whiteboardDatabase/create_whiteboard";
 
-const Home: FunctionComponent = () => {
+const Home = () => {
+  const location = useLocation();
+  const username = location.state?.username || "Guest"; // Default to 'Guest' if no username is available
+
   return (
     <div className="w-full min-h-[100vh] overflow-x-auto overflow-y-auto bg-[url('/public/home@3x.png')] bg-cover bg-no-repeat bg-[top] text-left text-[96px] text-black font-jaldi">
+      <UserWhiteboards />
       <Cards />
-      <h1 className="m-0 absolute top-[207px] left-[189px] text-inherit font-bold font-inherit inline-block w-[534px] h-[118px]">
-        Hello, User!
+      <h1 className='w-full absolute top-[140px] left-[180px] text-inherit font-bold font-inherit inline-block w-[534px] h-[118px]'>
+        Hello, {location.state.username}!
       </h1>
-      <h2 className="m-0 absolute top-[344px] left-[189px] text-13xl font-normal font-inherit inline-block w-[427px] h-[53px]">
+      <h2 className='m-0 absolute top-[344px] left-[189px] text-13xl font-normal font-inherit inline-block w-[427px] h-[53px]'>
         What will you map today?
       </h2>
       <NavBar />
