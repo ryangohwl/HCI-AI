15505372430a829b758d1006555249956fa5e682
Darrel <Darrelliew6@gmail.com>
2024-08-01 07:36:04 +0800

    2

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,22 +1,26 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
 import NewMindMapCard from "./NewMindMapCard";
 import OldMindMap from "./OldMindMap";
-import { useLocation } from "react-router-dom";
-import { useState, useEffect } from "react";
-import axios from "axios";
 
 export type CardsType = {
   className?: string;
   user_id: string;
 };
 
+interface Whiteboard {
+  _id: string;
+  // Add other relevant properties here
+}
+
 const Cards: FunctionComponent<CardsType> = ({ className = "", user_id }) => {
   const user = useLocation().state.user;
   const userId = user._id;
-  const username = user.username;
-  const whiteboards = user.whiteboards;
-  const [lastThreeWhiteboards, setLastThreeWhiteboards] = useState([]);
-  const [boardIds, setBoardIds] = useState([]);
+  const [lastThreeWhiteboards, setLastThreeWhiteboards] = useState<
+    Whiteboard[]
+  >([]);
+  const [boardIds, setBoardIds] = useState<string[]>([]);
 
   useEffect(() => {
     const fetchLastThreeWhiteboards = async () => {
@@ -24,7 +28,7 @@ const Cards: FunctionComponent<CardsType> = ({ className = "", user_id }) => {
         const response = await axios.get(
           `http://localhost:3000/whiteboard/lastthreewhiteboards/${userId}`
         );
-        setLastThreeWhiteboards(response.data.displayBoards);
+        setLastThreeWhiteboards(response.data.displayBoards); // Make sure your API matches this data structure
       } catch (error) {
         console.error("Error fetching whiteboards:", error);
       }
@@ -39,22 +43,14 @@ const Cards: FunctionComponent<CardsType> = ({ className = "", user_id }) => {
     }
   }, [lastThreeWhiteboards]);
 
-  // console.log(boardIds);
-
   return (
     <section
       className={`absolute top-[448px] left-[calc(50%_-_682.5px)] flex flex-row flex-wrap items-center justify-center gap-[55px] text-left text-xl text-black font-jaldi lg:flex-row lg:flex-wrap lg:items-center lg:justify-center md:flex-col md:items-center md:justify-center ${className}`}
     >
       <NewMindMapCard user_id={user_id} />
-      {boardIds.length > 0 && (
-        <OldMindMap boardId={boardIds[0]} userId={user_id} />
-      )}
-      {boardIds.length > 1 && (
-        <OldMindMap boardId={boardIds[1]} userId={user_id} />
-      )}
-      {boardIds.length > 2 && (
-        <OldMindMap boardId={boardIds[2]} userId={user_id} />
-      )}
+      {boardIds.map((id, index) => (
+        <OldMindMap key={index} boardId={id} userId={user_id} />
+      ))}
     </section>
   );
 };
