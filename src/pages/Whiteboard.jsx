b8f752d1217fe25eefaf362b800b34de71443093
Birthright00 <117997217+Birthright00@users.noreply.github.com>
2024-07-31 23:26:56 +0800

    Added HideToolbar (Where it hides specific buttons)

--- src/pages/Whiteboard.jsx
+++ src/pages/Whiteboard.jsx
@@ -1,22 +1,23 @@
-import React, { useState } from "react";
-import { Tldraw } from "tldraw";
+import React from "react";
+import { Tldraw } from "@tldraw/tldraw";
 import MyChatBot from "../components/chatbot/llm";
 import components from "../components/canvas/RightClickGenerate";
 import GetSelectedTexts from "../components/canvas/GetSelectedText";
- function Whiteboard() {
-	return (
-		<>
-          <div style={{ position: 'fixed', inset: 0 }}>
-            <Tldraw persistenceKey="Save-Whiteboard" components={components}>
-              <GetSelectedTexts />
-            </Tldraw>
-          </div>
-          <div className="chatbot">
-            <MyChatBot />
-            
-          </div>
-        </>
-	)
+import "../hideToolbar.css"; // Import the CSS file with the correct path
+
+function Whiteboard() {
+  return (
+    <>
+      <div style={{ position: 'fixed', inset: 0 }}>
+        <Tldraw persistenceKey="Save-Whiteboard" components={components}>
+          <GetSelectedTexts />
+        </Tldraw>
+      </div>
+      <div className="chatbot">
+        <MyChatBot />
+      </div>
+    </>
+  );
 }
 
-export default Whiteboard
+export default Whiteboard;
