71413d414f96ee16c75fddb63de91b9d1c39514e
Darrel <Darrelliew6@gmail.com>
2024-08-02 03:20:22 +0800

    whiteboards

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -26,7 +26,7 @@ const Cards: FunctionComponent<CardsType> = ({ className = "", user_id }) => {
     const fetchLastThreeWhiteboards = async () => {
       try {
         const response = await axios.get(
-          `{}}/${userId}`
+          `https://hci-ai-api.onrender.com/${userId}`
         );
         setLastThreeWhiteboards(response.data.displayBoards); // Make sure your API matches this data structure
       } catch (error) {
